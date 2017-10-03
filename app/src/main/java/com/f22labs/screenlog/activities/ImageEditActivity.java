fd6901433665e0a9d37e9a55906c398d0a0d2c7a
chandruf22 <chandrasekark@f22labs.com>
2017-10-03 19:29:38 +0530

    Added sticker view

--- app/src/main/java/com/f22labs/screenlog/activities/ImageEditActivity.java
+++ app/src/main/java/com/f22labs/screenlog/activities/ImageEditActivity.java
@@ -1,31 +1,25 @@
 package com.f22labs.screenlog.activities;
 
-import android.content.ClipData;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.util.Log;
-import android.view.DragEvent;
-import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.f22labs.screenlog.R;
 import com.f22labs.screenlog.utils.Utils;
-import com.f22labs.screenlog.views.ZoomTextView;
-import com.f22labs.screenlog.views.ZoomableView;
 import com.f2prateek.dart.Dart;
 import com.f2prateek.dart.InjectExtra;
-import com.meetic.dragueur.Direction;
-import com.meetic.dragueur.DraggableView;
+import com.xiaopo.flying.sticker.Sticker;
+import com.xiaopo.flying.sticker.StickerView;
+import com.xiaopo.flying.sticker.TextSticker;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -44,12 +38,16 @@ public class ImageEditActivity extends AppCompatActivity {
     ImageView imgAdd;
     @BindView(R.id.ll_input)
     LinearLayout llInput;
-    @BindView(R.id.frame_view)
-    FrameLayout frameView;
+
 
     float dX;
     float dY;
     int lastAction;
+    @BindView(R.id.sticker_view)
+    StickerView stickerView;
+    @BindView(R.id.constraint_lyt)
+    ConstraintLayout constraintLyt;
+    private boolean isInputLayoutEnabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,14 +58,55 @@ public class ImageEditActivity extends AppCompatActivity {
         Dart.inject(this);
 
         Utils.loadImage(this, imageAbsolutePath, imgScreenshot);
-    }
 
-    @OnClick({R.id.frame_view, R.id.img_add})
-    public void onClick(View v) {
 
-        switch (v.getId()) {
 
-            case R.id.frame_view:
+        stickerView.setOnStickerOperationListener(new StickerView.OnStickerOperationListener() {
+            @Override
+            public void onStickerAdded(@NonNull Sticker sticker) {
+
+            }
+
+            @Override
+            public void onStickerClicked(@NonNull Sticker sticker) {
+
+
+                Log.i("Clicked", "Sticker clicked");
+
+                stickerView.remove(sticker);
+            }
+
+            @Override
+            public void onStickerDeleted(@NonNull Sticker sticker) {
+
+            }
+
+            @Override
+            public void onStickerDragFinished(@NonNull Sticker sticker) {
+
+            }
+
+            @Override
+            public void onStickerZoomFinished(@NonNull Sticker sticker) {
+
+            }
+
+            @Override
+            public void onStickerFlipped(@NonNull Sticker sticker) {
+
+            }
+
+            @Override
+            public void onStickerDoubleTapped(@NonNull Sticker sticker) {
+
+            }
+        });
+
+
+        constraintLyt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
 
                 if (llInput.getVisibility() == View.VISIBLE) {
                     llInput.setVisibility(View.GONE);
@@ -75,7 +114,29 @@ public class ImageEditActivity extends AppCompatActivity {
                     llInput.setVisibility(View.VISIBLE);
                 }
 
-                break;
+
+                return false;
+            }
+        });
+    }
+
+
+
+    @OnClick({R.id.img_add})
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+//            case R.id.sticker_view:
+//
+//
+//                if (llInput.getVisibility() == View.VISIBLE) {
+//                    llInput.setVisibility(View.GONE);
+//                } else {
+//                    llInput.setVisibility(View.VISIBLE);
+//                }
+//
+//                break;
 
             case R.id.img_add:
 
@@ -89,58 +150,63 @@ public class ImageEditActivity extends AppCompatActivity {
 
     private void createNewTextView(String text) {
 
-        final FrameLayout.LayoutParams lparams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+//        final FrameLayout.LayoutParams lparams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+//
+//        lparams.gravity = Gravity.CENTER;
 
-        lparams.gravity = Gravity.CENTER;
-
-
-        final TextView textView = new TextView(this);
-        textView.setLayoutParams(lparams);
-        textView.setText(text);
-        textView.setGravity(Gravity.CENTER);
-        textView.setBackgroundColor(Color.CYAN);
-        textView.setPadding(20,20,20,20);
-
-
+//        StickerView stickerView = new StickerView(this);
+//
+//        stickerView.setLayoutParams(lparams);
+//
+//        stickerView.setBackgroundColor(Color.WHITE);
+//        stickerView.setLocked(false);
+//        stickerView.setConstrained(true);
 
 
-        textView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-
-                switch (event.getActionMasked()) {
-                    case MotionEvent.ACTION_DOWN:
-                        dX = view.getX() - event.getRawX();
-                        dY = view.getY() - event.getRawY();
-                        lastAction = MotionEvent.ACTION_DOWN;
-                        break;
-
-                    case MotionEvent.ACTION_MOVE:
-                        view.setY(event.getRawY() + dY);
-                        view.setX(event.getRawX() + dX);
-                        lastAction = MotionEvent.ACTION_MOVE;
-                        break;
-
-                    case MotionEvent.ACTION_UP:
-                        if (lastAction == MotionEvent.ACTION_DOWN)
-                            Toast.makeText(ImageEditActivity.this, "Clicked!", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    default:
-                        return false;
-                }
-                return true;
-
-            }
-        });
+        final TextSticker textSticker = new TextSticker(this);
+        textSticker.setText(text);
+        textSticker.setTextColor(Color.BLACK);
+        textSticker.setTextAlign(Layout.Alignment.ALIGN_CENTER);
+        textSticker.resizeText();
 
 
-        frameView.addView(textView);
+        stickerView.addSticker(textSticker);
+
+
+//        textView.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View view, MotionEvent event) {
+//
+//                switch (event.getActionMasked()) {
+//                    case MotionEvent.ACTION_DOWN:
+//                        dX = view.getX() - event.getRawX();
+//                        dY = view.getY() - event.getRawY();
+//                        lastAction = MotionEvent.ACTION_DOWN;
+//                        break;
+//
+//                    case MotionEvent.ACTION_MOVE:
+//                        view.setY(event.getRawY() + dY);
+//                        view.setX(event.getRawX() + dX);
+//                        lastAction = MotionEvent.ACTION_MOVE;
+//                        break;
+//
+//                    case MotionEvent.ACTION_UP:
+//                        if (lastAction == MotionEvent.ACTION_DOWN)
+//                            Toast.makeText(ImageEditActivity.this, "Clicked!", Toast.LENGTH_SHORT).show();
+//                        break;
+//
+//                    default:
+//                        return false;
+//                }
+//                return true;
+//
+//            }
+//        });
+
+
+//        stickerView.addView(stickerView);
 
     }
-
-
-
 
 
 }
